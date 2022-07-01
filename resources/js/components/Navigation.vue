35e1a91369d83da5505fbfd10fce72ab5b4803bc
Abdallah Bari <abdellah01win@gmail.com>
2022-07-01 19:41:40 +0100

    Creat form components, task controller, and link db

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -21,6 +21,7 @@
         <a class="mr-5 hover:text-gray-900">About</a>
         <a class="mr-5 hover:text-gray-900">Tasks</a>
         <a class="mr-5 hover:text-gray-900">Profile</a>
+        <input type="checkbox" class="toggle mr-5" checked />
       </nav>
       <button
         class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
